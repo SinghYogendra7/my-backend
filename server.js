7b86e83a83b7d25c51f6d2e4ad9743e14fe5c60f
Yogendra Singh <cyogendrasingh@gmail.com>
2025-07-31 01:51:41 +0530

    Trigger fresh redeploy with cleaned Railway vars

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ dns.lookup(process.env.DB_HOST, (err, address) => {
 // 🔌 DB Connection
 console.log('🔧 Connecting to DB with host:', process.env.DB_HOST);
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: 'interchange.proxy.rlwy.net',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
